f2cc99e013fcc2578cdf4e3deb8dfd596f950b65
Sathish Pasupunuri <sathish@pasupunuri.com>
2021-02-23 17:28:13 +0530

    Updated build

---
+++ public/precache-manifest.12d125a576770d543ac6ecc91cbc548b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d68753fdd1d201f7ec0c6ae11a39a19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "720454f05b182af84207",
+    "url": "/static/css/2.c86cd2d1.chunk.css"
+  },
+  {
+    "revision": "53151dda5df3f5fb37fd",
+    "url": "/static/css/main.fb5e79b7.chunk.css"
+  },
+  {
+    "revision": "720454f05b182af84207",
+    "url": "/static/js/2.14a12870.chunk.js"
+  },
+  {
+    "revision": "e8155266b3d88c09294eb89e68b95899",
+    "url": "/static/js/2.14a12870.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53151dda5df3f5fb37fd",
+    "url": "/static/js/main.6c401fba.chunk.js"
+  },
+  {
+    "revision": "cd5dcc5ca8f0e0af4a7e",
+    "url": "/static/js/runtime-main.ed2a8741.js"
+  }
+]);
