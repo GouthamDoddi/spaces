3a62001686a6d5e86b8944344154f0ffcb6285e8
Sathish Pasupunuri <sathish@pasupunuri.com>
2021-04-19 15:04:50 +0530

    added project eservices

--- src/services/project_eservices.rb
+++ src/services/project_eservices.rb
@@ -4,8 +4,8 @@ class App::Services::ProjectEservices < App::Services::Base
   def model; RevComplianceProject; end
 
   def list
-    cond = { project_id: rp[:entity_id]}
-    data = model.eager(:compliance_projects).order(Sequel.desc(:created_at)).where(cond.compact).map do |o|
+    cond = { project_id: rp[:project_id]}
+    data = model.order(Sequel.desc(:created_at)).where(cond).map do |o|
       hash = o.as_json
       hash[:progress] = o.progress
       hash[:issues_count] = o.issues_count
