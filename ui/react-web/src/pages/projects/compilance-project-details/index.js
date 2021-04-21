96475398bbd5752923b9056a3d03de354b5c941a
Vijay Sharma <vijay.sharma@appinventiv.com>
2021-04-21 07:44:17 +0000

    Feat/project mangement

--- ui/react-web/src/pages/projects/compilance-project-details/index.js
+++ ui/react-web/src/pages/projects/compilance-project-details/index.js
@@ -172,16 +172,14 @@ const CompilanceProjectDetails = () => {
             >
               Previous{' '}
             </button>
-            <button
-              className="btn_solid"
-              onClick={
-                step !== 4
-                  ? () => setStep((prevValue) => prevValue + 1)
-                  : undefined
-              }
-            >
-              Next{' '}
-            </button>
+            {step !== 4 && (
+              <button
+                className="btn_solid"
+                onClick={() => setStep((prevValue) => prevValue + 1)}
+              >
+                Next{' '}
+              </button>
+            )}
           </div>
 
           <div className="table_wrapper">
--- ui/react-web/src/pages/projects/compilance-project-details/submit-evidence.js
+++ ui/react-web/src/pages/projects/compilance-project-details/submit-evidence.js
@@ -10,12 +10,20 @@ import { ButtonLink } from '../../entities/details';
 import makeStore from '../../../store/make-store';
 import { useParams } from 'react-router';
 import { projectCategoryTypes } from '../../../store/master-data';
+import AutoComplete from '../../entities/auto_complete';
 
-const { load, create, update } = makeStore(({ project_id, id }) => `${project_id}/rev-compl-projects/${id ? `/${id}` : ''}`);
+const { load, create, update } = makeStore(
+  ({ project_id, id }) =>
+    `${project_id}/rev-compl-projects${id ? `/${id}` : ''}`
+);
+const { load: loadUsers } = makeStore(
+  ({ project_id }) => `rev-projects/${project_id}/users`
+);
 
 const SubmitEvidence = ({ setTableProps }) => {
   const { project_id } = useParams();
   const [id, setId] = useState();
+  const [users, setUsers] = useState([]);
   const [files, setFiles] = useState([]);
   const [errors, setErrors] = useState({});
   const [submitClicked, setSubmitClicked] = useState(false);
@@ -24,15 +32,15 @@ const SubmitEvidence = ({ setTableProps }) => {
   const [update, forceUpdate] = React.useState({});
 
   const [data, setData] = useState({
-    full_name: '',
+    spoc_ids: [],
     notes: '',
-    logo: null
+    logo: null,
   });
 
   const errorLabels = {
-    full_name: 'Full Name',
+    spoc_ids: 'Full Name',
     notes: 'Notes',
-    logo: 'Upload'
+    logo: 'Upload',
   };
 
   const isEmpty = (name, value) =>
@@ -40,7 +48,7 @@ const SubmitEvidence = ({ setTableProps }) => {
 
   const isInvalid = (name, value) => {
     switch (name) {
-      case 'full_name':
+      case 'spoc_ids':
         return isEmpty(name, value);
       case 'notes':
         return isEmpty(name, value);
@@ -53,7 +61,7 @@ const SubmitEvidence = ({ setTableProps }) => {
 
   const fetchFiles = (updated) => {
     forceUpdate([]);
-  }
+  };
 
   const handleEdit = (id) => {
     setId(id);
@@ -70,41 +78,30 @@ const SubmitEvidence = ({ setTableProps }) => {
   };
 
   useEffect(() => {
-    load({ project_id }, (data) => {
-      // data = Object.values(data).filter(({ type_id }) => type_id === 3);
+    loadUsers({ project_id }, (data) => {
+      setUsers(data);
 
-      // if (data[0]?.id) {
-      //   setId(data[0]?.id);
-      //   load({ id: data[0].id }, (data) =>
-      //     setData(
-      //       data.reduce((prevVal, { id, question, answer }) => {
-      //         return {
-      //           ...prevVal,
-      //           [id]: { question, answer: answer === 'yes' },
-      //         };
-      //       }, {})
-      //     )
-      //   );
-      // }
+      load({ project_id }, (data) => {
+        setId(data.id);
+        load({ id: data }, (data) => setData(data[0]));
 
-      // setTableProps({
-      //   rows: data,
-      //   renderCol: (colIndex, col) => {
-      //     if (colIndex === 0) {
-      //       return projectCategoryTypes[col]?.label;
-      //     }
-
-      //     if (colIndex === 2) {
-      //       return (
-      //         <ButtonLink onClick={() => handleEdit(col)}>
-      //           Edit Compliance Record
-      //         </ButtonLink>
-      //       );
-      //     }
-
-      //     return false;
-      //   },
-      // });
+        setTableProps({
+          rows: data,
+          renderCol: (colIndex, col) => {
+            if (colIndex === 0) {
+              return projectCategoryTypes[col]?.label;
+            }
+            if (colIndex === 2) {
+              return (
+                <ButtonLink onClick={() => handleEdit(col)}>
+                  Edit Compliance Record
+                </ButtonLink>
+              );
+            }
+            return false;
+          },
+        });
+      });
     });
   }, []);
 
@@ -124,13 +121,13 @@ const SubmitEvidence = ({ setTableProps }) => {
 
   const handleChange = ({ target: { value, name, type } }) => {
     if (type === 'checkbox') {
-      if (data[name].includes(value)) {
+      if (data[name].includes(parseInt(value))) {
         updateData(
           name,
-          data[name].filter((val) => val !== value)
+          data[name].filter((val) => val !== parseInt(value))
         );
       } else {
-        updateData(name, data[name].concat([value]));
+        updateData(name, data[name].concat([parseInt(value)]));
       }
 
       return;
@@ -156,15 +153,11 @@ const SubmitEvidence = ({ setTableProps }) => {
     }, false);
 
     if (!hasErrors) {
-      
     }
   };
 
-  const {
-    full_name,
-    notes,
-    logo
-  } = data;
+  const { spoc_ids, notes, logo } = data;
+
   return (
     <>
       <OuterRowFlex>
@@ -172,15 +165,11 @@ const SubmitEvidence = ({ setTableProps }) => {
           <div className="flex_col_sm_8">
             <div className="flex_row">
               <div className="flex_col_sm_5">
-                <LightGreyText>
-                  {'Evidence for '}
-                </LightGreyText>
+                <LightGreyText>{'Evidence for '}</LightGreyText>
               </div>
 
               <div className="flex_col_sm_6">
-                <BlueBorder>
-                  {'Login & Register'}
-                </BlueBorder>
+                <BlueBorder>{'Login & Register'}</BlueBorder>
               </div>
             </div>
           </div>
@@ -194,14 +183,14 @@ const SubmitEvidence = ({ setTableProps }) => {
               Name <mark>*</mark>
             </label>
             <div className="text_field_wrapper">
-              <input
-                type="text"
+              <AutoComplete
                 placeholder="Search and select user group"
-                name="full_name"
-                value={full_name}
+                name="spoc_ids"
+                values={spoc_ids}
                 onChange={handleChange}
+                error={errors.spoc_ids}
+                options={users.map(({ id, first_name }) => ({ value: id, label: first_name })) }
               />
-              <div className="error_messg">{errors.full_name}</div>
             </div>
           </div>
 
@@ -217,7 +206,9 @@ const SubmitEvidence = ({ setTableProps }) => {
                   format="dd-MM-yyyy"
                   id="date-picker-inline"
                   value={start_date}
-                  onChange={(date) => { setStartDate(date) }}
+                  onChange={(date) => {
+                    setStartDate(date);
+                  }}
                   autoOk={true}
                   KeyboardButtonProps={{ 'aria-label': 'change date' }}
                 />
@@ -234,7 +225,9 @@ const SubmitEvidence = ({ setTableProps }) => {
                   format="dd-MM-yyyy"
                   id="date-picker-inline"
                   value={end_date}
-                  onChange={(date) => { setEndDate(date) }}
+                  onChange={(date) => {
+                    setEndDate(date);
+                  }}
                   autoOk={true}
                   KeyboardButtonProps={{ 'aria-label': 'change date' }}
                 />
@@ -247,7 +240,12 @@ const SubmitEvidence = ({ setTableProps }) => {
               Notes <mark>*</mark>
             </label>
             <div className="text_field_wrapper">
-              <textarea name="description" value={notes} placeholder="Enter Notes" onChange={handleChange} />
+              <textarea
+                name="notes"
+                value={notes}
+                placeholder="Enter Notes"
+                onChange={handleChange}
+              />
               <span className="error_messg">{errors.notes}</span>
             </div>
           </div>
@@ -256,7 +254,7 @@ const SubmitEvidence = ({ setTableProps }) => {
         <div className="flex_col_sm_4">
           <div className="form_field_wrapper">
             <label className="form_label">
-            {'Upload Test Profile & Test Data'} <mark>*</mark>
+              {'Upload Test Profile & Test Data'} <mark>*</mark>
             </label>
             <DropZone
               files={files}
@@ -272,7 +270,6 @@ const SubmitEvidence = ({ setTableProps }) => {
             <span className="error_messg">{errors.logo}</span>
           </div>
         </div>
-
       </div>
 
       <div className="flex_col_sm_12 text-right">
@@ -284,22 +281,20 @@ const SubmitEvidence = ({ setTableProps }) => {
   );
 };
 
-
 const OuterRowFlex = styled.div`
-  margin-top:10px;
-  margin-bottom:10px;
-`
+  margin-top: 10px;
+  margin-bottom: 10px;
+`;
 
 const LightGreyText = styled.span`
-  color:#666666;
-`
+  color: #666666;
+`;
 
 const BlueBorder = styled.span`
-  color: #2680EB;
-  border: 2px solid #2680EB;
+  color: #2680eb;
+  border: 2px solid #2680eb;
   padding: 5px 15px;
   border-radius: 20px;
 `;
-
 
 export default SubmitEvidence;
