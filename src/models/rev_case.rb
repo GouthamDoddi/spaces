c4d5caedc51de2704b9de87cf2a7a25707178142
Sathish Pasupunuri <sathish@pasupunuri.com>
2021-04-25 09:38:27 +0530

    Updated category_ids

--- src/models/rev_case.rb
+++ src/models/rev_case.rb
@@ -4,7 +4,7 @@ class App::Models::RevCase < Sequel::Model
 
   def validate
     super
-    validates_presence [:entity_id, :project_id, :category_id]
+    validates_presence [:entity_id, :project_id, :category_ids]
     # validates_unique :email
     # validates_includes ROLES, :role
   end
